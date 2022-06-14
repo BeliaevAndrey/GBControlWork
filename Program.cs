672bcab8a80adb5db5b03667594ecb57a83852d8
Andrei Beliaev <abbot190414@gmail.com>
2022-06-14 13:39:28 +0300

    Program completed

--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+
+class Program
+{
+    public static string[] Chooser(string[] array, int maxLenth=3)  // Метод для обработки массива.
+    {
+        int count = 0;
+        string[] result = new string[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= maxLenth)
+            {
+                Array.Resize(ref result, result.Length + 1);
+                result[count] = array[i];
+                count++;
+            }
+        }
+        return result;
+    }
+
+    public static string ArrayToString(string[] array)  // Метод для формирования выходной строки.
+    {
+        return $"[ \"{string.Join("\", \"", array)}\" ]";
+    }
+
+    static void Main()
+    {
+    string[] inArray1 = { "hello", "2", "world", ":-)" };
+    string[] inArray2 = { "1234", "1567", "-2", "computer science" };
+    string[] inArray3 = { "Russia", "Denmark", "Kazan" };
+    string[] inArray4 = { "Russia", "Denmark", "Kazan", 
+                        "1234", "1567", "-2", "computer science", 
+                        "hello", "2", "world", ":-)", 
+                        "Bye", "hI", "Io", "Phobos", "Deimos"
+                        };
+
+    Console.WriteLine($"{ArrayToString(inArray1)} -> {ArrayToString(Chooser(inArray1))}");
+    Console.WriteLine($"{ArrayToString(inArray2)} -> {ArrayToString(Chooser(inArray2))}");
+    Console.WriteLine($"{ArrayToString(inArray3)} -> {ArrayToString(Chooser(inArray3))}");
+    Console.WriteLine($"{ArrayToString(inArray4)} -> {ArrayToString(Chooser(inArray4))}");
+
+    }
+
+}
